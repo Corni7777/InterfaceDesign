c88e5ecb625f91b77c354309d590fa890695f47d
Cornelius Schill <cornelius.robin.schill@hs-furtwangen.de>
2023-12-16 00:33:53 +0100

    fullscreen + break animation

--- block.js
+++ block.js
@@ -10,11 +10,19 @@ Interface.Rectangle = class {
         this.speed = Math.random() * 7 + 3;
         this.vx = Math.random() < 0.5 ? this.speed : -this.speed;
         this.vy = Math.random() * 2 - 1;
+        this.broken = false;
     }
 
     move() {
-        this.x += this.vx;
-        this.y += this.vy;
+        if (this.broken == false) {
+            this.x += this.vx;
+            this.y += this.vy;
+        }
+
+        if (this.broken == true) {
+            this.x;
+            this.y += 5;
+        }
     }
 
     hasCrossedCanvas() {
@@ -26,11 +34,35 @@ Interface.Rectangle = class {
         );
     }
 
+    breakBlock() {
+        this.broken = true;
+    }
 
 
     display() {
         var ctx = this.canvas.getContext("2d");
-        ctx.fillStyle = "white";
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        if (this.broken == false) {
+            ctx.fillStyle = "white";
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+        if (this.broken == true) {
+            var part1 = { x: this.x, y: this.y, width: this.width / 2, height: this.height };
+            var part2 = { x: this.x + this.width / 2, y: this.y, width: this.width / 2, height: this.height };
+            if (part1.width > 0) {
+                part1.width -= 5;
+                part1.x += -30;
+                part1.y -= -30;
+                ctx.fillStyle = "red";
+                ctx.fillRect(part1.x, part1.y, part1.width, part1.height);
+            }
+            if (part2.width > 0) {
+                part2.width -= 5;
+                part2.x += 30;
+                part2.y -= 30;
+                ctx.fillStyle = "red";
+                ctx.fillRect(part2.x, part2.y, part2.width, part2.height);
+            }
+        }
+
     }
 };
--- main.js
+++ main.js
@@ -2,11 +2,14 @@ var Interface;
 (function (Interface) {
     var circle;
     var rectangles = [];
+    var brokenblocks = []
     var timer;
+    var woosh = new Audio('woosh.wav');
 
     window.addEventListener("load", hndLoad);
 
     function hndLoad() {
+        document.querySelector("#fullscreen").addEventListener("click", toggleFullScreen)
 
         var playground = document.querySelector("#playground");
         timer = setInterval(createRectangle, 400);
@@ -42,6 +45,8 @@ var Interface;
             var hitted = circle.hit(rectangles[i])
             if (hitted == true) {
                 console.log("hit")
+                rectangles[i].breakBlock();
+                brokenblocks.push(rectangles[i])
                 rectangles.splice(i, 1);
                 i--;
                 break;
@@ -52,6 +57,10 @@ var Interface;
                 i--;
                 break;
             }
+        }
+        for (var i = 0; i < brokenblocks.length; i++) {
+            brokenblocks[i].move();
+            brokenblocks[i].display();
         }
         circle.display();
 
@@ -65,4 +74,74 @@ var Interface;
     }
 
 
-})(Interface || (Interface = {}));
+
+
+
+
+
+
+
+    var isFullScreen = false;
+    function toggleFullScreen() {
+        if (!isFullScreen) {
+            if (playground.requestFullscreen) {
+                playground.requestFullscreen();
+            } else if (playground.msRequestFullscreen) {
+                playground.msRequestFullscreen();
+            } else if (playground.mozRequestFullScreen) {
+                playground.mozRequestFullScreen();
+            } else if (playground.webkitRequestFullscreen) {
+                playground.webkitRequestFullscreen();
+            }
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            }
+        }
+    }
+    document.addEventListener("fullscreenchange", handleFullScreenChange);
+    document.addEventListener("webkitfullscreenchange", handleFullScreenChange);
+    document.addEventListener("mozfullscreenchange", handleFullScreenChange);
+    document.addEventListener("MSFullscreenChange", handleFullScreenChange);
+    function handleFullScreenChange() {
+        isFullScreen = !isFullScreen;
+
+        if (isFullScreen) {
+            // Setze die Größe des Canvas auf die Bildschirmgröße
+            playground.width = window.innerWidth;
+            playground.height = window.innerHeight;
+
+            document.addEventListener("mousemove", lockMouse);
+        } else {
+            // Setze die Größe des Canvas auf die ursprüngliche Größe zurück
+            playground.width = 890; // Passe dies an die ursprüngliche Breite an
+            playground.height = 560; // Passe dies an die ursprüngliche Höhe an
+
+            document.removeEventListener("mousemove", lockMouse);
+        }
+    }
+    function lockMouse(e) {
+        var rect = playground.getBoundingClientRect();
+        var mouseX = e.clientX - rect.left;
+        var mouseY = e.clientY - rect.top;
+
+        // Prüfe, ob sich der Mauszeiger außerhalb des Canvas befindet
+        if (mouseX < 0 || mouseY < 0 || mouseX > rect.width || mouseY > rect.height) {
+            // Setze den Mauszeiger innerhalb des Canvas zurück
+            var centerX = rect.width / 2;
+            var centerY = rect.height / 2;
+            window.dispatchEvent(new MouseEvent('mousemove', { clientX: centerX + rect.left, clientY: centerY + rect.top }));
+        }
+    }})
+
+
+
+
+
+    (Interface || (Interface = {}));
